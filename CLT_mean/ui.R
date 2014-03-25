5324bbf3f5cb7c996047abbae9b1f7652ca0b437
Mine Cetinkaya-Rundel <mine@stat.duke.edu>
2014-03-24 21:54:52 -0400

    fix URL to code in CLT prop and mean

--- CLT_mean/ui.R
+++ CLT_mean/ui.R
@@ -33,7 +33,7 @@ shinyUI(pageWithSidebar(
     br(),
     
     helpText(a(href="https://duke.qualtrics.com/SE/?SID=SV_3L8WjmwQo32cVk9", target="_blank", "Rate this app!")),
-    helpText(a(href="https://github.com/ShinyEd/ShinyEd/tree/master/CLT", target="_blank", "View code")),
+    helpText(a(href="https://github.com/ShinyEd/ShinyEd/tree/master/CLT_mean", target="_blank", "View code")),
     helpText(a(href="http://stat.duke.edu/~mc301/shiny/applets.html", target="_blank", "Check out other apps")),
     helpText(a(href="https://www.coursera.org/course/statistics", target="_blank", "Want to learn more for free?"))
     ),
