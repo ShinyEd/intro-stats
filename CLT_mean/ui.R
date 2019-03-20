70fc8487f89267253c132419953ca9e35731dc29
Revo Tesha <revogatustesha@gmail.com>
2019-03-20 00:01:20 -0400

    First attempt.

--- CLT_mean/ui.R
+++ CLT_mean/ui.R
@@ -1,44 +1,45 @@
 library(shiny)
 
-shinyUI(pageWithSidebar(
-  
+ui <- pageWithSidebar(
   headerPanel("Central Limit Theorem for Means"),
   
   sidebarPanel(
-    radioButtons("dist", "Parent distribution (population):",
-                 list("Normal" = "rnorm",
-                      "Uniform" = "runif",
-                      "Right skewed" = "rlnorm",
-                      "Left skewed" = "rbeta")),
-    br(),
-    
-    uiOutput("mu"),
-    uiOutput("sd"),
-    uiOutput("min"),
-    uiOutput("max"),
-    uiOutput("skew"),
-    
-    sliderInput("n", 
-                "Sample size:", 
-                value = 30,
-                min = 2, 
-                max = 500),
-    br(),
-    
-    sliderInput("k", 
-                "Number of samples:", 
-                value = 200,
-                min = 10, 
-                max = 1000),
-    br(),
-    
-    helpText(a(href="https://duke.qualtrics.com/SE/?SID=SV_3L8WjmwQo32cVk9", target="_blank", "Rate this app!")),
-    helpText(a(href="https://github.com/ShinyEd/ShinyEd/tree/master/CLT_mean", target="_blank", "View code")),
-    helpText(a(href="http://stat.duke.edu/~mc301/shiny/applets.html", target="_blank", "Check out other apps")),
-    helpText(a(href="https://www.coursera.org/course/statistics", target="_blank", "Want to learn more for free?"))
+    wellPanel( # write-up
+      radioButtons("dist", "Parent distribution (population):",
+                   list("Normal" = "rnorm",
+                        "Uniform" = "runif",
+                        "Right skewed" = "rlnorm",
+                        "Left skewed" = "rbeta")),
+      hr(), # write-up
+      
+      uiOutput("mu"),
+      uiOutput("sd"),
+      uiOutput("min"),
+      uiOutput("max"),
+      uiOutput("skew"),
+      
+      sliderInput("n",
+                  "Sample size:", 
+                  value = 30,
+                  min = 2,
+                  max = 500),
+      br(),
+      
+      sliderInput("k",
+                  "Number of samples:",
+                  value = 200,
+                  min = 10, 
+                  max = 1000)
     ),
     
-        
+    wellPanel(
+      helpText(a(href="https://duke.qualtrics.com/SE/?SID=SV_3L8WjmwQo32cVk9", target="_blank", "Rate this app!")),
+      helpText(a(href="https://github.com/ShinyEd/ShinyEd/tree/master/CLT_mean", target="_blank", "View code")),
+      helpText(a(href="http://stat.duke.edu/~mc301/shiny/applets.html", target="_blank", "Check out other apps")),
+      helpText(a(href="https://www.coursera.org/course/statistics", target="_blank", "Want to learn more for free?"))
+    )),
+  
+  
   
   mainPanel(
     plotOutput("pop.dist"),
@@ -51,4 +52,4 @@ shinyUI(pageWithSidebar(
     br(),
     div(h5(textOutput("CLT.descr"), align = "center"))
   )
-))
+)
