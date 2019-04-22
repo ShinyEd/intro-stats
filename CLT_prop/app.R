5dd59dee7e6be017bfe6cfd9feda21c6475bb735
Mine Cetinkaya-Rundel <mine@stat.duke.edu>
2019-04-22 15:40:00 -0400

    Remove two file app, and rename single file app to be deployed

--- CLT_prop/app.R
+++ CLT_prop/app.R
@@ -1,10 +1,7 @@
 # Load packages -----------------------------------------------------
 library(shiny)
-library(openintro)
 library(gridExtra)
-library(BHH2)
 library(tidyverse)
-
 
 # Define UI ---------------------------------------------------------
 
